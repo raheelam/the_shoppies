c95737d54e227c0cb4a0a9245a1008444324bc0d
raheela <raheelamoha@gmail.com>
2021-01-12 22:20:18 +0200

    changed column sizes

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -2,24 +2,25 @@ import './MovieItem.css';
 
 
 const MovieItem = ({ movie, onMovieSelect, action }) => {
+  const noimg = "https://st.depositphotos.com/1824465/1436/v/600/depositphotos_14366609-stock-illustration-vector-film-reel.jpg";
   return (
     <div  className="movie-item item">
       <img
         alt={movie.title}
         className="ui image"
-        src={movie.Poster}
+        src={(movie.Poster !== "N/A")?movie.Poster:noimg}
       />
-      <div className="content">
+      <div className="content movie-content">
         <div className="header">{movie.Title}</div>
        
         
-        <div class="meta">
-        <div class="cinema"><span>Year Released: {movie.Year}</span></div>
+        <div className="meta">
+        <div className="cinema"><span>Year Released: {movie.Year}</span></div>
         </div>
         
 
         
-        <div class="extra nominate-btn">
+        <div className="extra nominate-btn">
         <div onClick={() =>onMovieSelect(movie)}
         
          className={`ui left floated button ${((movie.nominated==="true") && (action==="Nominate"))? 'disabled' : ''}`}>
